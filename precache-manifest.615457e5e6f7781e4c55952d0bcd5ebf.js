efc8ea07b044e7ff6be03504e4644c98532f4ab4
travischantravis <chancheukyin4463@gmail.com>
2020-03-12 18:29:03 -0700

    Updates

---
+++ precache-manifest.615457e5e6f7781e4c55952d0bcd5ebf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "944bcc181760660ed9348b3f6a0a9c07",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "5aa76038bb39e87e9fdf",
+    "url": "/recipe-app/static/css/main.cefe0c23.chunk.css"
+  },
+  {
+    "revision": "3b7f70a0c872a148e2ca",
+    "url": "/recipe-app/static/js/2.8a409502.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/recipe-app/static/js/2.8a409502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aa76038bb39e87e9fdf",
+    "url": "/recipe-app/static/js/main.ac981000.chunk.js"
+  },
+  {
+    "revision": "887dbc60425b31b5d4ea",
+    "url": "/recipe-app/static/js/runtime-main.2006017e.js"
+  }
+]);
